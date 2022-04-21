1f883c31814c0d218fa633eede2c9feeab064777
AdaKeita <98240696+adakeita@users.noreply.github.com>
2022-04-22 00:06:58 +0200

    added files

---
+++ js/music-list.js
@@ -0,0 +1,38 @@
+let allMusic = [
+   {
+	  name: "Don't Start Now",
+	  artist: "Dua Lipa",
+	  img: "music1",
+	  src:"music1"
+   },
+   {
+	  name: "Control",
+	  artist: "Halsey",
+	  img: "music2",
+	  src:"music2"
+   },
+   {
+	  name: "Heathens",
+	  artist: "21 pilots",
+	  img: "music3",
+	  src:"music3"
+   },
+   {
+	  name: "Old Town Road",
+	  artist: "LilNas X",
+	  img: "music4",
+	  src:"music4"
+   },
+   {
+	  name: "Demons",
+	  artist: "Imagine Dragons",
+	  img: "music5",
+	  src:"music5"
+   },
+   {
+	  name: "The Revolution Will Not Be Televised",
+	  artist: "Gill Scott-Heron",
+	  img: "music6",
+	  src:"music6"
+   },
+];
